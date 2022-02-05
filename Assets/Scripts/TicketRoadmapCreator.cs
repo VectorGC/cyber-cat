9dbc65cadf6c1319b0c061ebc2b000d5a9ccf6a7
Karim <karim.kimsanbaev@gmail.com>
2022-02-05 11:35:25 +0300

    Merge remote-tracking branch 'origin/#10-LayoutGroupsForNodes' into 0.1.0

--- Assets/Scripts/TicketRoadmapCreator.cs
+++ Assets/Scripts/TicketRoadmapCreator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.UI;
 
 public interface ITicketRoadmapCreator
 {
@@ -15,10 +16,12 @@ public abstract class BaseTicketRoadmapCreator : MonoBehaviour, ITicketRoadmapCr
 
 public class TicketRoadmapCreator : BaseTicketRoadmapCreator
 {
-    public TaskNodeView TaskNodeViewPrefab;
-    public Transform TaskNodeContainer;
+    [SerializeField] private TaskNodeView _taskNodeViewPrefab;
+    [SerializeField] private Transform _taskNodeContainer;
+    [SerializeField] private HorizontalLayoutGroup _horizontalLayoutGroupPrefab;
 
     private Canvas _canvas;
+    private NonBinarTree _tree;
 
     private void Start()
     {
@@ -27,10 +30,65 @@ public class TicketRoadmapCreator : BaseTicketRoadmapCreator
 
     public override void CreateRoadmap(IReadOnlyCollection<ITicket> tickets)
     {
-        foreach (var ticket in tickets)
+        if (tickets.Count == 0)
         {
-            var taskNodeView = Instantiate(TaskNodeViewPrefab,  Vector3.zero, Quaternion.identity, TaskNodeContainer);
-            taskNodeView.UpdateView(ticket);
+            return;
+        }
+        _tree = new NonBinarTree(tickets.ElementAt(0));
+        AddChildrenToTree(tickets, 0, 0, _tree.RootNode);
+        TaskNodeView taskNodeView;
+        var layout = Instantiate(_horizontalLayoutGroupPrefab, Vector3.zero, Quaternion.identity, _taskNodeContainer);
+        taskNodeView = Instantiate(_taskNodeViewPrefab, Vector3.zero, Quaternion.identity, layout.gameObject.transform);
+        taskNodeView.UpdateView(tickets.ElementAt(0));
+        CreateLayoutsWithNodes();
+    }
+
+    private void CreateLayoutsWithNodes()
+    {
+        List<List<NodeTree>> NodesPerLevelOfTree = new List<List<NodeTree>>();
+        List<NodeTree> nodes = new List<NodeTree>();
+        int level = 0;
+        while (true)
+        {
+            nodes = _tree.GetNodesOfLevel(level);
+            if (nodes.Count == 0)
+            {
+                break;
+            }
+            NodesPerLevelOfTree.Add(new List<NodeTree>());
+            NodesPerLevelOfTree[level] = new List<NodeTree>();
+            NodesPerLevelOfTree[level].AddRange(nodes);
+            level++;
+        }
+
+        TaskNodeView taskNodeView;
+        HorizontalLayoutGroup layout;
+        for (int i = 0; i < level; i++)
+        {
+            nodes = NodesPerLevelOfTree[i];
+            layout = Instantiate(_horizontalLayoutGroupPrefab, Vector3.zero, Quaternion.identity, _taskNodeContainer);
+            foreach (var node in nodes)
+            {
+                taskNodeView = Instantiate(_taskNodeViewPrefab, Vector3.zero, Quaternion.identity, layout.gameObject.transform);
+                taskNodeView.UpdateView(node.Ticket);
+            }
+        }
+    }
+
+    private void AddChildrenToTree(IReadOnlyCollection<ITicket> tickets, int indexOfTicket, int offset, NodeTree curNode)
+    {
+        var ticket = tickets.ElementAt(indexOfTicket);
+        if (ticket.CountOfChildren == 0)
+        {
+            return;
+        }
+
+        int indexOfNewParentNode = 0;
+        for (int i = 0; i < ticket.CountOfChildren; i++)
+        {
+            indexOfNewParentNode = indexOfTicket + i + offset + 1;
+            var ticket2 = tickets.ElementAt(indexOfNewParentNode);
+            AddChildrenToTree(tickets, indexOfNewParentNode, ticket.CountOfChildren - i - 1, curNode.AddNode(ticket2));
         }
     }
 }
