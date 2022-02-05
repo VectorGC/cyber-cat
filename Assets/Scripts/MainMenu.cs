d9fd1d531944c6d41343df2a6f2ad20f80fcdaac
Karim <karim.kimsanbaev@gmail.com>
2022-02-05 11:37:00 +0300

    Merge remote-tracking branch 'origin/ButtonSound(#11)' into 0.1.0
    # Conflicts:
    #	Assets/Scripts/MainMenu.cs

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text greetingstext;
+    public void Start()
+    {
+        greetingstext.text = ("Привет, " + /*Имя игрока из БД?*/ "PlayerName" + "!");
+    }
+    public void Run()
+    {
+        SceneManager.LoadScene("GlobalMap");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
