9a9105dcce847e1a1a1354d076d23d94b8a585d5
Karpik <artem19051664@gmail.com>
2022-03-20 12:10:27 +0300

    Add Tutorial scene and Start trigger to introduce player in game

--- Assets/StartTrigger.cs
+++ Assets/StartTrigger.cs
@@ -2,17 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartTrigger : MonoBehaviour
+public class StartTrigger : Trigger
 {
+    [Header("Приветствие")]
+    [SerializeField] private string _title1;
+    [SerializeField] private string _description1;
+    [SerializeField] private string _buttonText1;
+
+    [Header("Что вообще делать")]
+    [SerializeField] private string _title2;
+    [SerializeField] private string _description2;
+    [SerializeField] private string _buttonText2;
+
+    [Header("Что делать сейчас")]
+    [SerializeField] private string _title3;
+    [SerializeField] private string _description3;
+    [SerializeField] private string _buttonText3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
+        transform.position = Player.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Hello()
     {
-        
+        string[] buttonText = new string[]
+        {
+            _buttonText1
+        };
+        UnityEngine.Events.UnityAction[] action = new UnityEngine.Events.UnityAction[]
+        {
+            Goal
+        };
+        ModalPanel.Instance.MessageBos(ModalPanel.Instance.InfoIcon, _title1, _description1, true, 1, action, buttonText);
+    }
+
+    private void Goal()
+    {
+        string[] buttonText = new string[]
+        {
+            _buttonText1
+        };
+        UnityEngine.Events.UnityAction[] action = new UnityEngine.Events.UnityAction[]
+        {
+            WhatToDoNow
+        };
+        ModalPanel.Instance.MessageBos(ModalPanel.Instance.InfoIcon, _title2, _description2, true, 1, action, buttonText);
+    }
+
+    private void WhatToDoNow()
+    {
+        string[] buttonText = new string[]
+        {
+            _buttonText3
+        };
+        UnityEngine.Events.UnityAction[] action = new UnityEngine.Events.UnityAction[]
+        {
+            StartMessageOK
+        };
+        ModalPanel.Instance.MessageBos(ModalPanel.Instance.InfoIcon, _title3, _description3, true, 1, action, buttonText);
+    }
+
+    private void StartMessageOK()
+    {
+
+    }
+
+    public override void Enter()
+    {
+        Hello();
     }
 }
