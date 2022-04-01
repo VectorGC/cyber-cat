b3473fc3c694c3b5abb8bc9c94dd256c5c78121b
Snegirev <slavick.snegirev@icloud.com>
2022-04-01 17:58:55 +0300

    Issue-(#40)
    Making lights and add objects in lab

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,36 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class DoorTrigger : MonoBehaviour
+//{
+//    public Door door;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+
+//    public void OnTriggerEnter(Collider collision)
+//    {
+//        if (collision.tag == "Player")
+//        {
+//            door.Open();
+//        }
+//    }
+
+//    public void OnTriggerExit(Collider collision)
+//    {
+//        if (collision.tag == "Player")
+//        {
+//            door.Close();
+//        }
+//    }
+//}
