8d2b3a48f186d7927dcf6b371228e00b82ea5304
Karim <karim.kimsanbaev@gmail.com>
2022-04-03 11:52:16 +0300

    Disable 'create account' button & use old api without email confirmation
    Disable 'create account' button & use old api without email confirmation #2

--- Assets/Scripts/RestAPIWrapper/Endpoint.cs
+++ Assets/Scripts/RestAPIWrapper/Endpoint.cs
@@ -2,7 +2,7 @@ namespace RestAPIWrapper
 {
     public static class Endpoint
     {
-        public const string URI = "https://kee-reel.com/cyber-cat/v2";
+        public const string URI = "https://kee-reel.com/cyber-cat";
         public const string LOGIN = URI + "/login";
         public const string REGISTER = URI + "/register";
     }
