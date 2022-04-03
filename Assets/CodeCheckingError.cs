1d21d0ff01d4dd42babe5dd42f314683366ef11b
Karim <karim.kimsanbaev@gmail.com>
2022-04-03 11:50:24 +0300

    Fix error message on compile error

--- Assets/CodeCheckingError.cs
+++ Assets/CodeCheckingError.cs
@@ -2,11 +2,17 @@ using Newtonsoft.Json;
 
 public class CodeCheckingError : ICodeConsoleMessage
 {
+    [JsonProperty("msg")] public string Msg { get; set; }
     [JsonProperty("expected")] public string Expected { get; set; }
 
     public override string ToString()
     {
-        return $"Не правильный вывод, ожидается '{Expected}'";
+        if (string.IsNullOrEmpty(Msg))
+        {
+            return $"Не правильный вывод, ожидается '{Expected}'";
+        }
+
+        return Msg;
     }
 
     public string Message => ToString();
