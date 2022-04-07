e53afbebae0809537c336c9afc1ae82d88bebcc7
Slermo <lazysheeplazy@gmail.com>
2022-04-08 00:03:53 +0300

    ErrorsDescrption
    Added description at Console win

--- Assets/CodeCheckingError.cs
+++ Assets/CodeCheckingError.cs
@@ -2,17 +2,24 @@ using Newtonsoft.Json;
 
 public class CodeCheckingError : ICodeConsoleMessage
 {
+    [JsonProperty("stage")] public string Stage { get; set; }
     [JsonProperty("msg")] public string Msg { get; set; }
     [JsonProperty("expected")] public string Expected { get; set; }
+    [JsonProperty("params")] public string Params { get; set; }
+    [JsonProperty("result")] public string Result { get; set; }
 
     public override string ToString()
     {
-        if (string.IsNullOrEmpty(Msg))
+        
+        switch(Stage)
         {
-            return $"Не правильный вывод, ожидается '{Expected}'";
+            case "test":
+                return $"Данные для теста: '{Params}'\nВаш вывод: {Result}\nОжидается: '{Expected}'";
+            case "build":
+                return Msg;
+            default: 
+                return "Неизвестная ошибка. Обратитесь к админу";
         }
-
-        return Msg;
     }
 
     public string Message => ToString();
