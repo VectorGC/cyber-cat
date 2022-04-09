b873cf0262c5c3bbb8e8aedb68dd13c25cb71933
Karim <karim.kimsanbaev@gmail.com>
2022-04-10 00:34:21 +0300

    Use prefab in tutorial & fix hack mode observer & add simple nav mesh for game scene

--- Assets/Scripts/Legacy_do_not_use_it/HackModeObserver.cs
+++ Assets/Scripts/Legacy_do_not_use_it/HackModeObserver.cs
@@ -1,17 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HackModeObserver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         var isHackModePressed = Input.GetKeyDown(KeyCode.Q);
         if (isHackModePressed)
@@ -19,5 +10,10 @@ public class HackModeObserver : MonoBehaviour
             Debug.Log("Hack mode pressed");
             GameMode.HackMode = GameMode.HackMode == Mode.Default ? Mode.HackMode : Mode.Default;
         }
+    }
+
+    private void OnDestroy()
+    {
+        GameMode.HackMode = Mode.Default;
     }
 }
