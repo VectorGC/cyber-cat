9a2ab1127c314ea1d632c891e039473e84c4039f
Karpik <artem19051664@gmail.com>
2022-04-13 20:51:29 +0300

    Add quest disabler
    add this to quest giver and set this in taskunit

---
+++ Assets/QuestDisabler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TasksData;
+
+public class QuestDisabler : MonoBehaviourObserver<ITaskData>
+{
+    [SerializeField] private Trigger _triggerToDisable;
+
+    public override void OnCompleted()
+    {
+        Destroy(this);
+    }
+
+    public override void OnError(Exception error)
+    {
+        throw error;
+    }
+
+    public override void OnNext(ITaskData value)
+    {
+        if (value.IsSolved is true)
+        {
+            _triggerToDisable.enabled = false;
+        }
+        
+    }
+}
