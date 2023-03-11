f43a0ced42bd35d844bc0fac9df8afe9b19c8a23
Karim <karim.kimsanbaev@gmail.com>
2023-03-11 13:42:40 +0300

    Use swagger only in development & add comments

--- backend/CyberCatServer/ApiGateway/Program.cs
+++ backend/CyberCatServer/ApiGateway/Program.cs
@@ -3,18 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.MapGet("/", (context) =>
+// Если мы в режиме разработки. В Release это работать не будет.
+if (app.Environment.IsDevelopment())
 {
-    context.Response.Redirect("/swagger");
-    return Task.CompletedTask;
-});
+    // Показываем API спецификацию через swagger.
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapGet("/", (context) =>
+    {
+        context.Response.Redirect("/swagger");
+        return Task.CompletedTask;
+    });
 
+    // Подробные ошибки в режиме разработки.
+    app.UseDeveloperExceptionPage();
+}
+
+// Размечаем методы контроллеров как ендпоинты.
 app.MapControllers();
 
 app.Run();
