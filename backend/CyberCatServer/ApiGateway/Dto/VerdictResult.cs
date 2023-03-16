c9ad87489fff443a4d2e25cb4e13b95be25ac595
Karim <karim.kimsanbaev@gmail.com>
2023-03-16 19:00:06 +0300

    Cleanup

--- backend/CyberCatServer/ApiGateway/Dto/VerdictResult.cs
+++ backend/CyberCatServer/ApiGateway/Dto/VerdictResult.cs
@@ -5,5 +5,5 @@ namespace ApiGateway.Dto;
 public class VerdictResult
 {
     [JsonPropertyName("error")] public int Error { get; set; }
-    [JsonPropertyName("error_data")] public VerdictError ErrorData { get; set; } = null!;
+    [JsonPropertyName("error_data")] public VerdictError? ErrorData { get; set; } = null!;
 }
