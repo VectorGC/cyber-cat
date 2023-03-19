00bae7beea8d3ae034a9439da0c2513c01c57464
Karim <karim.kimsanbaev@gmail.com>
2023-03-20 00:45:27 +0300

    Remove users for migrations

--- backend/CyberCatServer/ApiGateway/Repositories/Migrations/AddDevPlayers.cs
+++ backend/CyberCatServer/ApiGateway/Repositories/Migrations/AddDevPlayers.cs
@@ -21,59 +21,10 @@ public class AddDevPlayers : IUserRepositoryMigration
         {
             new UserDto()
             {
-                Email = "Alkon",
-                Name = "Иван",
-                LastName = "Алкон",
-                Password = "1"
-            },
-            new UserDto()
-            {
-                Email = "Lukashev",
-                Name = "Михаил",
-                LastName = "Лукашев",
-                Password = "12"
-            },
-            new UserDto()
-            {
-                Email = "Karpik",
-                Name = "Артем",
-                LastName = "Карпинский",
-                Password = "123"
-            },
-            new UserDto()
-            {
-                Email = "Snegirev",
-                Name = "Снегирев",
-                LastName = "Святослав",
-                Password = "1234"
-            },
-            new UserDto()
-            {
-                Email = "Pekush",
-                Name = "Даниил",
-                LastName = "Пекуш",
-                Password = "12345"
-            },
-            new UserDto()
-            {
-                Email = "Ustinovskiy",
-                Name = "Устиновский",
-                LastName = "Георгий",
-                Password = "123456"
-            },
-            new UserDto()
-            {
-                Email = "Mazhaicev",
-                Name = "Евгений",
-                LastName = "Мажайцев",
-                Password = "1234567"
-            },
-            new UserDto()
-            {
                 Email = "Kimsanbaev",
                 Name = "Карим",
                 LastName = "Кимсанбаев",
-                Password = "12345678"
+                Password = "123456"
             },
         };
 
