db14be92617ff14ead1ed521510312cd916fa53c
Karim <karim.kimsanbaev@gmail.com>
2023-04-08 15:01:16 +0300

    Use wsl for min gw

--- backend/CyberCatServer/CompilerServiceAPI/Controllers/CompilerController.cs
+++ backend/CyberCatServer/CompilerServiceAPI/Controllers/CompilerController.cs
@@ -21,6 +21,7 @@ namespace CompilerServiceAPI.Controllers
         public IActionResult CompileCode(string code)
         {
             //Запрос для компиляции кода
+            code = "#include <stdio.h>\nint main() { printf(\"Hello from my library!\"); }";
 
             //Создаем cpp файл с кодом из запроса
             using (StreamWriter writer = System.IO.File.CreateText("code.cpp"))
@@ -29,11 +30,17 @@ namespace CompilerServiceAPI.Controllers
             }
             
             //Компилируем cpp файл с помощью mingw
-            var res = _commandService.RunDockerCommandWithOverrideOutput("x86_64-w64-mingw32-g++", "code.cpp -Wall -o code.exe -static-libgcc -static-libstdc++");
+            var res = _commandService.RunDockerCommandWithOverrideOutput("wsl", "x86_64-w64-mingw32-g++ code.cpp -Wall -Werror -o code.exe -static-libgcc -static-libstdc++");
+
+            if (!string.IsNullOrEmpty(res))
+            {
+                return Ok(res);
+            }
 
             //Запускаем полученный exe с помощью wine и получаем строку из стандартного потока вывода процесса
-            //var res = _commandService.RunDockerCommandWithOverrideOutput("wine", "code.exe");
-            return Ok(res);
+            //var res2 = _commandService.RunDockerCommandWithOverrideOutput("wine", "code.exe");
+            var res2 = _commandService.RunDockerCommandWithOverrideOutput("code.exe", "");
+            return Ok(res2);
         }
     }
 }
