8a87f9493af5286638314a64bf41189d6a88193e
Karpik <artem19051664@gmail.com>
2023-04-16 16:41:48 +0300

    Add restoring password tests

---
+++ cyber-cat/Assets/RestAPIWrapper/Editor Tests/RestoringPasswordTests.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using Authentication;
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace RestAPIWrapper.EditorTests
+{
+    public class RestoringPasswordTests
+    {
+        [Test]
+        public void WhenRestoringPassword_AndServerless_ThenTaskIsNotNull()
+        {
+            //Arrange
+            var login = "Karpik";
+            var pass = "123";
+
+            //Act
+            var uniTask = new Serverless.RestAPIServerless().RestorePassword(login, pass);
+
+            //Assert
+            Assert.IsNotNull(uniTask);
+        }
+
+        [Test]
+        public void WhenRestoringPassword_AndLoginAndPAsswordAreCorrect_ThenTaskIsNotNull()
+        {
+            //Arrange
+            var login = "Karpik";
+            var pass = "123";
+
+            //Act
+            var uniTask = RestAPI.Instance.RestorePassword(login, pass);
+
+            //Assert
+            Assert.IsNotNull(uniTask);
+        }
+
+        [Test]
+        public void WhenRestoringPassword_AndLoginAndPAsswordAreCorrectAndServer_ThenTaskIsNotNull()
+        {
+            //Arrange
+            var login = "Karpik";
+            var pass = "123";
+
+            //Act
+            var uniTask = new Server.RestAPIServer().RestorePassword(login, pass);
+
+            //Assert
+            Assert.IsNotNull(uniTask);
+        }
+    }
+}
