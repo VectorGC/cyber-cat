11b04d5d2a9ec6bba5835efbcc67cc7321114f68
Karim <karim.kimsanbaev@gmail.com>
2023-06-01 19:12:33 +0300

    Polish

--- cyber-cat/Assets/Scripts/GameManager/GameManager.cs
+++ cyber-cat/Assets/Scripts/GameManager/GameManager.cs
@@ -1,4 +1,3 @@
-using ApiGateway.Client;
 using AuthService;
 using Repositories.TaskRepositories;
 using ServerAPI;
@@ -7,23 +6,21 @@ using Services.AuthService;
 
 public class GameManager
 {
-    public readonly ILocalStorageService LocalStorage = new PlayerPrefsStorage();
-    public readonly ITaskRepository TaskRepository;
-    public readonly IAuthService AuthService;
-
-    private readonly IClient _serverClient;
+    public ILocalStorageService LocalStorage { get; } = new PlayerPrefsStorage();
+    public ITaskRepository TaskRepository { get; }
+    public IAuthService AuthService { get; }
 
     private GameManager()
     {
-        _serverClient = ServerAPIFacade.Create();
+        var serverClient = ServerAPIFacade.Create();
         var token = LocalStorage.Player?.Token.Value;
         if (!string.IsNullOrEmpty(token))
         {
-            _serverClient.AddAuthorizationToken(token);
+            serverClient.AddAuthorizationToken(token);
         }
 
-        TaskRepository = new TaskRepositoryProxy(_serverClient);
-        AuthService = new AuthServiceProxy(_serverClient);
+        TaskRepository = new TaskRepositoryProxy(serverClient);
+        AuthService = new AuthServiceProxy(serverClient);
     }
 
     public static GameManager Instance
