3cf179ee9b8788cf8b2af52ce9c71e86d7788a10
Karim <karim.kimsanbaev@gmail.com>
2023-06-15 15:59:03 +0300

    Revert "Added iostream input/output tests"
    This reverts commit 52334e38b77f034348b1b1572fc2e0d99423c9f5.

---
+++ backend/CyberCatServer/CppLauncherService/CppLauncherService/10iyn2kk.cpp
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main() { int a; int b; scanf("%d%d", &a, &b); printf("2"); }
---
+++ backend/CyberCatServer/CppLauncherService/CppLauncherService/vmjglbjj.cpp
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main() { int a; int b; int c; scanf("%d%d", &a, &b); scanf("%d", &c); }
