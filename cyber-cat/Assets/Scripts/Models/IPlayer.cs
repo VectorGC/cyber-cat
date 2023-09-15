d674f205a32bc3b60ff5574b31b0118f18df140d
Karim Kimsanbaev <karim.kimsanbaev@gmail.com>
2023-09-15 13:23:20 +0300

    Remove useless libraries

--- cyber-cat/Assets/Scripts/Models/IPlayer.cs
+++
@@ -1,8 +0,0 @@
-namespace Models
-{
-    public interface IPlayer
-    {
-        string Name { get; }
-        ITokenSession Token { get; }
-    }
-}
--- cyber-cat/Assets/Scripts/NeedToImplements/MonoBehaviourObserver.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-public abstract class MonoBehaviourObserver<T> : MonoBehaviour, IObserver<T>
-{
-    public abstract void OnCompleted();
-    public abstract void OnError(Exception error);
-    public abstract void OnNext(T value);
-}
