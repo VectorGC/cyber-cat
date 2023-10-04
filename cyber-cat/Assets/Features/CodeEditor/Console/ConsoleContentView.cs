bd1975c7624e8e77b82c957add6e61616716258b
Karim Kimsanbaev <karim.kimsanbaev@gmail.com>
2023-10-05 01:37:20 +0300

    Add answer summary

--- cyber-cat/Assets/Features/CodeEditor/Console/ConsoleContentView.cs
+++ cyber-cat/Assets/Features/CodeEditor/Console/ConsoleContentView.cs
@@ -1,9 +1,11 @@
+using Shared.Models.Models.Verdicts;
 using UniMob;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ConsoleContentView : LifetimeUIBehaviour<CodeEditorState>
 {
+    [SerializeField] private Text _answerSummary;
     [SerializeField] private TestCasesView _testCasesView;
     [SerializeField] private Text _emptyLabel;
     [Atom] public override CodeEditorState State { get; set; }
@@ -23,6 +25,30 @@ public class ConsoleContentView : LifetimeUIBehaviour<CodeEditorState>
         else
         {
             _emptyLabel.gameObject.SetActive(false);
+        }
+
+        if (State?.Section is ResultSection resultSection)
+        {
+            switch (resultSection.SelectedTestCaseVerdict)
+            {
+                case SuccessTestCaseVerdict success:
+                    _answerSummary.text = "Верный ответ";
+                    _answerSummary.color = Color.green;
+                    _answerSummary.gameObject.SetActive(true);
+                    break;
+                case FailureTestCaseVerdict failure:
+                    _answerSummary.text = "Неверный ответ";
+                    _answerSummary.color = Color.red;
+                    _answerSummary.gameObject.SetActive(true);
+                    break;
+                default:
+                    _answerSummary.gameObject.SetActive(false);
+                    break;
+            }
+        }
+        else
+        {
+            _answerSummary.gameObject.SetActive(false);
         }
     }
 }
